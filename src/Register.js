e5def4f37d5e8802d21722a7323ed09a051902af
harichandana-opp <harichandana.p15@gmail.com>
2022-09-13 11:32:04 +0530

    my first commit

--- src/Register.js
+++ src/Register.js
@@ -15,11 +15,11 @@ function Register() {
             <div id="login-page">
                 <div className="form-box">
                <form id ='register' className="input">
-                <input type='text' className="in-field" placeholder="First Name" required/><br/><br/>
-                <input type='text' className="in-field" placeholder="Last Name" required/><br/><br/>
-                <input type='email' className="in-field" placeholder="Email Id" required/><br/><br/>
-                <input type='password' className="in-field" placeholder="Enter Password" required/><br/><br/>
-                <input type='password' className="in-field" placeholder="Confirm Password" required/><br/><br/>
+                <input type='text'name="firstname" className="in-field" placeholder="First Name" required/><br/><br/>
+                <input type='text' name="lastname" className="in-field" placeholder="Last Name" required/><br/><br/>
+                <input type='email' name="emailid" className="in-field" placeholder="Email Id" required/><br/><br/>
+                <input type='password' name="password" className="in-field" placeholder="Enter Password" required/><br/><br/>
+                <input type='password' name="confirmpass" className="in-field" placeholder="Confirm Password" required/><br/><br/>
                 <input type="checkbox" classsName="check-box" />I agree to the terms and conditions<br/><br/>
                 <button type ='submit' class='sub-btn'>Register</button>
                 
